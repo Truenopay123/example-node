bd563c7362f40bd6ee0090691c12c56d2db14fcc
Bryan Axel Cortes Cortes <bryanaxelcortescortes@gmail.com>
2026-04-10 23:52:44 -0600

    fix: correct routes path in app.js

--- fast-api-dev/app.js
+++ fast-api-dev/app.js
@@ -3,9 +3,9 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
-var indexRouter = require('../fast-api-dev/routes/index')
-var usersRouter = require('../fast-api-dev/routes/users')
-var itemsRouter = require('../fast-api-dev/routes/items')
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var itemsRouter = require('./routes/items')
 
 var app = express()
 
